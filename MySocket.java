4ee1c6750cce7035fed67d81c250dc4b6754072f
papiboi99 <papiboi99.sad@gmail.com>
2020-12-31 21:30:11 +0100

    Implementada parte del cliente

--- MySocket.java
+++ MySocket.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class MySocket {
@@ -75,5 +72,4 @@ public class MySocket {
 
         }
     }
-
 }
